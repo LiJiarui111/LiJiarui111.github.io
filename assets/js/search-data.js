2776c7339d65c1ac19fca643b70be308d4fbf0a1
LiJiarui111 <geraldlee0528@gmail.com>
2025-01-24 22:45:01 +0000

    Deploying to gh-pages from @ LiJiarui111/LiJiarui111.github.io@7af0e7270e9c4c618566b1fa2233ff871b650d05 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -473,13 +473,6 @@ ninja.data = [{
           window.open("https://twitter.com/Jiarui_Li_", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
